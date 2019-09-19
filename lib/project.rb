a7404a938835de96f1152d497a3e1098778a230a
wcomfort <54148522+wcomfort@users.noreply.github.com>
2019-09-19 14:36:15 -0400

    Update project.rb

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,13 @@
 class Project
-    attr_reader :title, :backers 
+    attr_reader :title, #:backers 
     def initialize(title)
         @title = title
         @backers = []
     end
+    
+    #def backers
+    #    
+    #end
 
     def add_backer(backer)
         self.backers << backer
